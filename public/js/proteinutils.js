f9bb30f17d5894071ca4995efda5cde7a6d23063
axyorah <elvira.bozileva@gmail.com>
2021-03-22 15:02:00 +0100

    refactor: minor refactor in protein utils

--- public/js/proteinutils.js
+++ public/js/proteinutils.js
@@ -10,14 +10,49 @@ function setUnitLen() {
         params.unitVec[2] * params.unitVec[2];
 }
 
+function populateMainProteinChain( protein, ele ) {
+    const proteinMain = paths['objPath'];
+
+    for (let xyz of proteinMain) {
+        const sphere = new THREE.Mesh(ele.geometry, ele.material);
+        
+        const x = params.scale * xyz[0];
+        const y = params.scale * xyz[1];
+        const z = params.scale * xyz[2];
+        
+        sphere.position.set(x, y, z);        
+        protein.add(sphere);
+    }
+}
+
+function populateProteinTube( protein, ele ) {
+    const helixCenters = paths['helixCenters'];     
+    const helixNormals = paths['helixNormals'];
+
+    for (let i = 0; i < helixCenters.length; i++) {
+        const sphere = new THREE.Mesh(ele.geometry, ele.material);
+
+        const xyz = helixCenters[i];
+        const n = helixNormals[i];
+        
+        const x = params.scale * (xyz[0] + n[0] * params.rad);
+        const y = params.scale * (xyz[1] + n[1] * params.rad);
+        const z = params.scale * (xyz[2] + n[2] * params.rad);
+        
+        sphere.position.set(x, y, z);
+        protein.add(sphere);
+    }
+}
+
 function addProteinToScene( scene ) {
     // define 'building blocks'
     setUnitLen();
     const rad = params.unitLen * params.scale * 0.01; // radius of a 'building block' sphere
     const sphereGeometry = new THREE.SphereGeometry(rad, 16, 16);
     const sphereMaterial = new THREE.MeshPhongMaterial( { shininess: 4 } );
-	sphereMaterial.color.setHex( 0x6723C7 );
+	sphereMaterial.color.setHex( 0x6723F7 );
 	sphereMaterial.specular.setRGB( 0.5, 0.5, 0.5 );
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
     // delete old protein if present
     for (let i=0; i < scene.children.length; i++) {
@@ -31,35 +66,10 @@ function addProteinToScene( scene ) {
     let protein = new THREE.Object3D();
     protein.name = 'Protein';
 
-    const proteinMain = paths['objPath'];
-    const helixCenters = paths['helixCenters'];     
-    const helixNormals = paths['helixNormals'];
-
-    for (let xyz of proteinMain) {
-        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        
-        const x = params.scale * xyz[0];
-        const y = params.scale * xyz[1];
-        const z = params.scale * xyz[2];
-        
-        sphere.position.set(x, y, z);        
-        protein.add(sphere);
-    }
-
-    for (let i = 0; i < helixCenters.length; i++) {
-        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-        const xyz = helixCenters[i];
-        const n = helixNormals[i];
-        
-        const x = params.scale * (xyz[0] + n[0] * params.rad);
-        const y = params.scale * (xyz[1] + n[1] * params.rad);
-        const z = params.scale * (xyz[2] + n[2] * params.rad);
-        
-        sphere.position.set(x, y, z);        
-        protein.add(sphere);
-    }
-
+    populateMainProteinChain(protein, sphere);
+    populateProteinTube(protein, sphere);    
+    
+    // adjust protein position/rotation
     protein.rotation.set(params.xRot, params.yRot, params.zRot);
     protein.position.set(params.xPos, params.yPos, params.zPos);
     scene.add(protein);
